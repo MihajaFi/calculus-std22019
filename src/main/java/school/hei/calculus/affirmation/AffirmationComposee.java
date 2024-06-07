2b76599b70a5bc9498c04f36870993bc9a2c3ed1
Fifaliana <hei.fifaliana@gmail.com>
2024-06-07 14:54:40 +0300

    feat: add affirmation composee

---
+++ src/main/java/school/hei/calculus/affirmation/AffirmationComposee.java
@@ -0,0 +1,22 @@
+package school.hei.calculus.affirmation;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import school.hei.calculus.conjonction.Conjonction;
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+public final class AffirmationComposee extends Affirmation {
+    private Affirmation gauche;
+    private Affirmation droite;
+    private Conjonction conjonction;
+
+    @Override
+    public boolean estVrai() {
+        return conjonction.evalue(gauche.estVrai(), droite.estVrai());
+    }
+
+}
