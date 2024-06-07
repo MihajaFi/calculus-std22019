d219778e489974baf029ce9c959277d0b5f76d23
Fifaliana <hei.fifaliana@gmail.com>
2024-06-07 15:13:21 +0300

    chore: add additional test for affirmation

--- src/test/java/school/hei/calculus/affirmation/AffirmationTest.java
+++ src/test/java/school/hei/calculus/affirmation/AffirmationTest.java
@@ -14,4 +14,41 @@ class AffirmationTest {
         assertFalse(composition.estVrai());
 
     }
+
+    @Test
+    void test_Lou_est_beau_donc_Lou_est_pauvre() {
+        Affirmation verite_1 = new Vérité("Lou est beau");
+        Affirmation mensonge_1 = new Mensonge("Lou est pauvre");
+        Affirmation composition = new AffirmationComposee(verite_1, mensonge_1, Conjonction.DONC);
+        assertFalse(composition.estVrai());
+        System.out.println(composition.toString());
+    }
+
+    @Test
+    void test_Lou_est_pauvre_donc_Lou_est_généreux() {
+        Affirmation mensonge_1 = new Mensonge("Lou est pauvre");
+        Affirmation verite_1 = new Vérité("Lou est généreux");
+        Affirmation composition = new AffirmationComposee(mensonge_1, verite_1, Conjonction.DONC);
+        assertTrue(composition.estVrai());
+        System.out.println(composition.toString());
+    }
+    @Test
+
+    void test_Lou_est_beau_ou_Lou_est_généreux_donc_Lou_est_pauvre() {
+
+        Affirmation verite_1 = new Vérité("Lou est beau");
+
+        Affirmation verite_2 = new Vérité("Lou est généreux");
+
+        Affirmation composition1 = new AffirmationComposee(verite_1, verite_2, Conjonction.OU);
+
+        Affirmation mensonge_1 = new Mensonge("Lou est pauvre");
+
+        Affirmation composition2 = new AffirmationComposee(composition1, mensonge_1, Conjonction.DONC);
+
+        assertFalse(composition2.estVrai());
+
+    }
+
+
 }
